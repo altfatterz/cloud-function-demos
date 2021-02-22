a75663869ff888beeb029bec6b2b3a23adc4d60a
Zoltan Altfatter <altfatterz@gmail.com>
2021-02-22 22:16:55 +0100

    add request parameter

--- hello-world-java/src/main/java/com/example/HelloWorldJavaCloudFunction.java
+++ hello-world-java/src/main/java/com/example/HelloWorldJavaCloudFunction.java
@@ -4,13 +4,14 @@ import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
 public class HelloWorldJavaCloudFunction implements HttpFunction {
 
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
-        BufferedWriter writer = response.getWriter();
-        writer.write("Hello world!");
+        String name = request.getFirstQueryParameter("name").orElse("world");
+        var writer = new PrintWriter(response.getWriter());
+        writer.printf("Hello %s!", name);
     }
 }
